63e95cf067c73abdc8f07637539a493c1d5a5c8f
Dominick Meglio <dmeglio@gmail.com>
2022-11-05 00:30:59 -0400

    Added tzlocal as a dependency

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dcmeglio/pykeurig",
     packages=setuptools.find_packages(),
-    install_requires=["httpx", "signalrcore"],
+    install_requires=["httpx", "signalrcore","tzlocal"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
